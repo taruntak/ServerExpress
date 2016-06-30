71ed580c8b68ac4de9ee733930dc815de62e8684
Tarun Tak <Tarun.Tak@happiestminds.com>
2016-06-30 14:59:05 +0530

    minor changes

--- config/config-dev.js
+++ config/config-dev.js
@@ -8,9 +8,5 @@ var config = {};
 config.HOST_IP = 'localhost';
 config.HOST_PORT = 4000;
 
-// MONGODB VARIABLES
-config.DB_HOST = 'localhost';
-config.DB_PORT = 27017;
-config.DB_URL = 'mongodb://localhost:27017/';
 
 module.exports = config;
