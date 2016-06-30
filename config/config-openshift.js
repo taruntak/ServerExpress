4309215085ffaa06995ae7743c4e09c3933308d0
Tarun Tak <Tarun.Tak@happiestminds.com>
2016-06-30 17:17:10 +0530

    minor commit

--- config/config-openshift.js
+++ config/config-openshift.js
@@ -5,7 +5,7 @@ var env = process.env;
 var config = {};
 
 // HOST VARIABLES
-config.HOST_IP = env.OPENSHIFT_NODEJS_IP;
-config.HOST_PORT = env.OPENSHIFT_NODEJS_PORT;
+config.HOST_IP = env.OPENSHIFT_NODEJS_IP || 8080;
+config.HOST_PORT = env.OPENSHIFT_NODEJS_PORT || '127.0.0.1';
 
 module.exports = config;
