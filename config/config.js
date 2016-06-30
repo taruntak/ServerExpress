17346ff01156480b4c5313c945b249591d2bfbe8
Tarun Tak <Tarun.Tak@happiestminds.com>
2016-06-30 15:23:01 +0530

    minor changes

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ var development = require('./config-dev');
 var env = process.env;
 
 function getConfig() {
-    if(env.NODE_ENV && env.NODE_ENV === 'production') {
+    // if(env.NODE_ENV && env.NODE_ENV === 'production') {
         return openshift;
-    } else {
-        return development;
-    }
+    // } else {
+    //     return development;
+    // }
 }
 
 module.exports = getConfig();
